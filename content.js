75bf1460b7bd995e3468d6964295e51220e1912b
Agastya Kumar Yadav <124435030+agastya3636@users.noreply.github.com>
2024-01-24 11:43:19 +0530

    working

--- content.js
+++ content.js
@@ -1,77 +1,107 @@
-// content.js
-// Listen for messages from the extension's background script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // Check if the message action is "getPageData"
-    if (request.action === "scanButton") {
-      console.log("Received action to getPageData",request);
-      let url=request.tabUrl
-      console.log("content url",url)
-      try {
-        // Extract data from the current page
-        console.log("Getting page data...1111");
-        const elements = document.querySelectorAll("body *");
-        console.log("Total elements found:", elements.length);
-  
-        // Filter and map elements to get non-empty, visible text
-        let pageData = Array.from(elements)
-          .filter(
-            (element) =>
-              element.innerText &&
-              element.offsetWidth > 0 &&
-              element.offsetHeight > 0
-          )
-          .map((element) => element.innerText.trim());
-  
-        console.log("Extracted data:", pageData.length);
-        for(let i=0;i<pageData.length;i++)
-        {
-          //replace all special character?
-          pageData[i]=pageData[i].replace(/₹/g, '_');
+  if (request.action === "detectAds") {
+      const ul = request.tabUrl;
+      const adResult = detectAds();
+      const freetrialResult = freetrial();
 
-        }
-        
-      
-        // Check if any data is extracted before sending it to the background script
-        if (pageData.length === 0) {
-          console.log("No data extracted from the page.");
-        } else {
-          console.log("Sending data to background script:", pageData);
-  
-          // Send the extracted data to the background script
-          chrome.runtime.sendMessage(
-            {
-              action: "sendDataToServer",
-              data: pageData,
-              url:url,
-            },
-            function (response) {
-              // Handle the response from the background script
-              if (chrome.runtime.lastError) {
-                console.error(
-                  "Error in sending message to background script:",
-                  chrome.runtime.lastError.message
-                );
-              } else {
-                console.log("Response from background script:", response);
-                //send response to popup.js?
-              }
-            }
-          );
-        }
-  
-        // Send a success response back to the sender
-        sendResponse({
-          status: "Success",
-          message: "Data processing completed in content script",
-        });
-      } catch (error) {
-        // Handle errors and send an error response
-        console.error("Error in content script:", error);
-        sendResponse({ status: "Error", message: error.message });
-      }
-  
-      // Ensure that the sendResponse callback is invoked asynchronously
-      return true;
-    }
+      chrome.runtime.sendMessage({
+          action: "adDetectionResult",
+          adResult: adResult,
+          freetrialResult: freetrialResult,
+          tabUrl: ul,
+      });
+  }
   });
-  
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+  if (request.action === "scanButton") {
+      console.log("Received action to getPageData", request);
+      let url = request.tabUrl;
+      console.log("content url", url);
+      try {
+          const elements = document.querySelectorAll("body *");
+          console.log("Total elements found:", elements.length);
+
+          let pageData = Array.from(elements)
+              .filter(
+                  (element) =>
+                      element.innerText &&
+                      element.offsetWidth > 0 &&
+                      element.offsetHeight > 0
+              )
+              .map((element) => element.innerText.trim());
+
+          console.log("Extracted data:", pageData.length);
+          for (let i = 0; i < pageData.length; i++) {
+              // Replace all special characters
+              pageData[i] = pageData[i].replace(/₹/g, '_');
+          }
+
+          if (pageData.length === 0) {
+              console.log("No data extracted from the page.");
+          } else {
+              console.log("Sending data to background script:", pageData);
+
+              chrome.runtime.sendMessage(
+                  {
+                      action: "sendDataToServer",
+                      data: pageData,
+                      url: url,
+                  },
+                  function (response) {
+                      if (chrome.runtime.lastError) {
+                          console.error(
+                              "Error in sending message to background script:",
+                              chrome.runtime.lastError.message
+                          );
+                      } else {
+                          console.log("Response from background script:", response);
+                      }
+                  }
+              );
+          }
+
+          sendResponse({
+              status: "Success",
+              message: "Data processing completed in content script",
+          });
+      } catch (error) {
+          console.error("Error in content script:", error);
+          sendResponse({ status: "Error", message: error.message });
+      }
+
+      return true;
+  }
+});
+
+function detectAds() {
+  const adPatterns = ['adsbygoogle', 'google_ad'];
+  const potentialAds = document.querySelectorAll(adPatterns.map(pattern => `[class*="${pattern}"], [id*="${pattern}"], [src*="${pattern}"]`).join(','));
+
+  return potentialAds.length > 0;
+}
+
+function freetrial() {
+  const freetrials = ['freetrial', 'try for free'];
+  const potentialFreetrials = document.querySelectorAll(freetrials.map(pattern => `[class*="${pattern}"], [id*="${pattern}"], [src*="${pattern}"],[href*="${pattern}"],[a*="${pattern}"]`).join(','));
+  const pageContent = document.documentElement.innerText.toLowerCase();
+  let b = 0;
+  const autor = ['autorenew', 'autorenewal'];
+  for (let i = 0; i < freetrials.length; i++) {
+      b = b + pageContent.includes(freetrials[i]);
+  }
+  let c = 0;
+  for (const element of autor) {
+      c = c + pageContent.includes(element);
+  }
+
+  return potentialFreetrials.length > 0 || (b > 0 && c > 0);
+}
+
+
+
+
+
+
+
--- popup.js
+++ popup.js
@@ -1,27 +1,57 @@
-//popup.js
+// popup.js
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, { action: "detectAds", tabUrl: tabs[0].url });
+});
 
-document.getElementById("loadData").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "adDetectionResult") {
+      const adResult = message.adResult;
+      const freetrialResult = message.freetrialResult;
+      const wrong = '<svg class="iconns" xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#fa0000" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
+      const right = '<svg class="iconss" xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 448 512"><path fill="#009912" d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>';
+      const adStatus = adResult ? (right + 'Ads detected!') : (wrong + ' No ads detected.');
+      const freetrialStatus = freetrialResult ? (right + ' Detected') : (wrong + ' Not Detected.');
+
+      const adsResultElement = document.getElementById("ads_result");
+      const statusElement = document.getElementById("trials_result");
+
+      adsResultElement.innerHTML = '';
+      statusElement.innerHTML = '';
+      const adsTempContainer = document.createElement("div");
+      const freetrialTempContainer = document.createElement("div");
+      adsTempContainer.innerHTML = adStatus;
+      freetrialTempContainer.innerHTML = freetrialStatus;
+      while (adsTempContainer.firstChild) {
+          adsResultElement.appendChild(adsTempContainer.firstChild);
+      }
+
+      while (freetrialTempContainer.firstChild) {
+          statusElement.appendChild(freetrialTempContainer.firstChild);
+      }
+  }
+});
+document.getElementById("scanButton").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(
-        tabs[0].id,
-        { action: "getPageData",
-        tabUrl: tabs[0].url
-     },
-        (response) => {
-          if (chrome.runtime.lastError) {
-            document.getElementById("dataDisplay").textContent =
-              "Error: " + chrome.runtime.lastError.message;
-            return;
+          tabs[0].id,
+          {
+              action: "scanButton",
+              tabUrl: tabs[0].url
+          },
+          (response) => {
+              if (chrome.runtime.lastError) {
+                  document.getElementById("datadisplay").textContent =
+                      "Error: " + chrome.runtime.lastError.message;
+                  return;
+              }
+              console.log("hi" + response);
+              // Check if response contains data property
+              if (response && response.data) {
+                  document.getElementById("datadisplay").textContent = response.data.join("\n");
+              } else {
+                  document.getElementById("datadisplay").textContent = "No data received.";
+              }
           }
-          console.log("hi"+response);
-          // Check if response contains data property
-          if (response && response.data) {
-            document.getElementById("dataDisplay").textContent = response.data.join("\n");
-        } else {
-            document.getElementById("dataDisplay").textContent = "No data received.";
-        }
-        }
       );
-    });
   });
-  
+});
